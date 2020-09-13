225d53862964c7d8ac30b26c50c3211a9aeff69f
johndoe434 <68118162+johndoe434@users.noreply.github.com>
2020-09-13 12:49:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="discordpy-replit-heroku",
-    version="0.1.1",
+    version="0.1.2",
     author="John Doe",
     author_email="johndo3@repl.email",
     description="Hosting your repl based discord.py bot on Heroku to keep it running INDEFINETLY",
