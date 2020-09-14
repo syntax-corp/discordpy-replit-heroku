99f094a79c58deff0a0e4ee6328260770ee90321
johndoe434 <68118162+johndoe434@users.noreply.github.com>
2020-09-14 16:55:49 -0400

    Update server.py

--- bot/server.py
+++ bot/server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 from threading import Thread
 
 app = Flask('')
 
 @app.route('/')
 def main():
-  return 'yey i exist'
+  return render_template('index.html')
 
 def run():
   app.run(host='0.0.0.0', port=8080)
