309f27f31c1d9db3fa306b579d76209bcffd974c
johndoe434 <68118162+johndoe434@users.noreply.github.com>
2020-09-18 13:51:08 -0400

    fixed errors

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,8 @@
-import discord, os, pynacl, dnspython, server
+import discord
+import os
+#import pynacl
+#import dnspython
+import server
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
